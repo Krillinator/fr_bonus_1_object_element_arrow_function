7a3eb567ae0e699e3a8fe9545086d52aa30b5fa2
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2025-02-14 09:38:50 +0100

    Add: Arrow Function deep dive example

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,6 +5,10 @@ export function Welcome() {
   const [user, setUser] = useState<User>({ username: "Benny", password: "123" })
   const [count, setCount] = useState<number>(0)
 
+  function addToCount(x: number, event: React.MouseEvent) {
+    setCount(x)
+  }
+
   return (
     <div>
       <p>Hello world :)</p>
@@ -12,6 +16,14 @@ export function Welcome() {
       {/* ReactNode does not Accept OBJECTS */}
       <p>{count}</p> {/* number is OK */}
       <p>{JSON.stringify(user)}</p> {/* object is NOT OK */}
+      {/* Arrow Function Example */}
+      <button
+        onClick={(event) => {
+          addToCount(20, event)
+        }}
+      >
+        Add to Count +1
+      </button>
     </div>
   )
 }
